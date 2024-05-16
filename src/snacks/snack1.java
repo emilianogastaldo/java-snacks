b77fc0bb316f0aedda83f33259e797c2e700f3a5
emilianogastaldo <emiliano0303@gmail.com>
2024-05-16 14:06:30 +0200

    snack 1

---
+++ src/snacks/snack1.java
@@ -0,0 +1,16 @@
+package snacks;
+
+import java.util.Scanner;
+
+public class snack1 {
+    public static void main(String[] args) {
+        /*Snack1
+        Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.  */
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inserisci un numero");
+        int num = scanner.nextInt();
+        if(num % 2 != 0) System.out.println(num+1);
+        else System.out.println(num);
+    }
+}
